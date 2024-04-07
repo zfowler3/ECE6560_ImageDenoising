ad1724cb000ba0d35e0a97ffaaa037b4750ab37e
zoe <zfowler3@gatech.edu>
2024-04-07 16:12:43 -0400

    Adding noise funcs

--- add_noise.py
+++ add_noise.py
@@ -9,7 +9,19 @@ def gaussian_noise(input_img, mean, var):
     noisy_img = gauss + input_img
     return noisy_img
 def salt_and_pepper(input_img):
-    num_pixels = random.randint(int(input_img.shape[0]/4), int(input_img.shape[0]/2))
+    noisy_img = np.copy(input_img)
+    num_pixels_salt = random.randint(int(input_img.shape[0]/10), int(input_img.shape[0]/4))
+    # salt
+    for s in range(num_pixels_salt):
+        row = random.randint(0, input_img.shape[0]-1)
+        col = random.randint(0, input_img.shape[-1]-1)
+        noisy_img[row, col] = 255
+    num_pixels_pepper = random.randint(int(input_img.shape[0]/10), int(input_img.shape[0]/4))
+    # pepper
+    for p in range(num_pixels_pepper):
+        row = random.randint(0, input_img.shape[0]-1)
+        col = random.randint(0, input_img.shape[-1]-1)
+        noisy_img[row, col] = 0
 
     return noisy_img
 
