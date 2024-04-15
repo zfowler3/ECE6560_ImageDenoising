74258147fa9fdefe82e80b8eec21b089a18ad29a
zoe <zfowler3@gatech.edu>
2024-04-15 18:44:51 -0400

    visualize

--- Perona_Malik_Base.py
+++ Perona_Malik_Base.py
@@ -18,7 +18,45 @@ def custom_coeff(I, k):
     return (0.04+0.2*np.exp(-inside))
 
 
-def diffusion(img, iters, k, lam=0.12, coeff=1):
+def diffusion_(img, iters, k, lam=0.12, coeff=1):
+    #http://image.diku.dk/imagecanon/material/PeronaMalik1990.pdf
+    img = img / 255
+    img_new = np.zeros(img.shape, dtype=img.dtype)
+
+    for step in range(iters):
+
+        NORTH = img[:-2, 1:-1] - img[1:-1, 1:-1]
+
+        SOUTH = img[2:, 1:-1] - img[1:-1, 1:-1]
+
+        EAST = img[1:-1, 2:] - img[1:-1, 1:-1]
+
+        WEST = img[1:-1, :-2] - img[1:-1, 1:-1]
+
+        if coeff == 1:
+            img_new[1:-1, 1:-1] = img[1:-1, 1:-1] + \
+                                 (diffusion_coeff1(NORTH, k) * NORTH +
+                                        diffusion_coeff1(SOUTH, k) * SOUTH +
+                                        diffusion_coeff1(EAST, k) * EAST +
+                                        diffusion_coeff1(WEST, k) * WEST)
+        elif coeff == 2:
+            img_new[1:-1, 1:-1] = img[1:-1, 1:-1] + \
+                                 (diffusion_coeff2(NORTH, k) * NORTH +
+                                        diffusion_coeff2(SOUTH, k) * SOUTH +
+                                        diffusion_coeff2(EAST, k) * EAST +
+                                        diffusion_coeff2(WEST, k) * WEST)
+        else:
+            img_new[1:-1, 1:-1] = img[1:-1, 1:-1] + \
+                                 (custom_coeff(NORTH, k) * NORTH +
+                                        custom_coeff(SOUTH, k) * SOUTH +
+                                        custom_coeff(EAST, k) * EAST +
+                                        custom_coeff(WEST, k) * WEST)
+        img = img_new
+
+    return img
+
+def diffusion(img, iters, k, coeff=1):
+    #http://image.diku.dk/imagecanon/material/PeronaMalik1990.pdf
     img = img / 255
     img_new = np.zeros(img.shape, dtype=img.dtype)
 
